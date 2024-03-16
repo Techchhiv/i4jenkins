79521943826fc83d184cd1187345778a83ce3c78
Techchhiv <127384271+Techchhiv@users.noreply.github.com>
2024-03-16 14:49:45 +0700

    revert back the error

--- src/main/java/i4/gic/Main.java
+++ src/main/java/i4/gic/Main.java
@@ -1,12 +1,8 @@
 package i4.gic;
 
 public class Main {
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-
-=======
     public static void main(String[] args) {
-        System.out.println("Testing");
->>>>>>> dc6c7ae71e3a5ab7b5bc19b90a850da844c0485f
+        System.out.println("Hello world!");
+        
     }
 }
